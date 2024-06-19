80079221951a1488a022f903c4f9dc54f9f9e7cd
Aerendyll <143167807+Aerendyll@users.noreply.github.com>
2024-06-19 03:02:02 +0200

    feat: loading-skeleton fix: mobile responsive header button - change title & favicon

--- src/components/ChampionImage.jsx
+++ src/components/ChampionImage.jsx
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { useInView } from "react-intersection-observer";
+import "react-loading-skeleton/dist/skeleton.css";
+import Skeleton from "react-loading-skeleton";
 
 const ChampionImage = ({ championName }) => {
   const baseUrl = "https://ddragon.leagueoflegends.com/cdn/img/champion/tiles/";
   const [imageUrl, setImageUrl] = useState(null);
-  const { ref, inView } = useInView({
-    triggerOnce: true,
-    threshold: 0.1,
-  });
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const loadChampionImage = async () => {
@@ -23,7 +21,10 @@ const ChampionImage = ({ championName }) => {
         for (let i = 0; i < variations.length; i++) {
           const response = await fetch(baseUrl + variations[i]);
           if (response.ok) {
-            setImageUrl(baseUrl + variations[i]);
+            setTimeout(() => {
+              setImageUrl(baseUrl + variations[i]);
+              setLoading(false);
+            }, 1000);
             return;
           }
         }
@@ -31,18 +32,23 @@ const ChampionImage = ({ championName }) => {
         throw new Error(`Could not load image for ${championName}`);
       } catch (error) {
         console.error("Error loading champion image:", error);
+        setLoading(false);
       }
     };
 
-    if (inView) {
-      loadChampionImage();
-    }
-  }, [inView, championName]);
+    loadChampionImage();
+  }, [championName]);
 
   return (
-    <div className="card-content" ref={ref}>
-      {imageUrl ? <img src={imageUrl} alt={championName} /> : <p>Loading...</p>}
-      <p>{championName}</p>
+    <div className="card-content">
+      {loading ? (
+        <Skeleton height={200} width={200} />
+      ) : imageUrl ? (
+        <img src={imageUrl} alt={championName} />
+      ) : (
+        <p>Image not available</p>
+      )}
+      <p className="name-p">{championName}</p>
     </div>
   );
 };
